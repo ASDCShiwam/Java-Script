04683edb35b0ffa2e3c2809f1afca0fb43717432
Shiwam Yadav <developershiwam1569@gmail.com>
2025-07-12 04:52:22 +0000

    variables-const let var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const acountID = 8856
+let acountEmail= "developershiwam1569@gmail.com"
+var acountPassword="12345"
+acountCity="New Dlhi"
+
+let acountState;
+
+//acountID=1256 // not allowed because its declared as const and cant be change
+//console.log(acountID);
+
+acountEmail="shiwamnumo3@gmail.com"
+acountPassword="dbchjdgf"
+acountCity="Varanasi"
+
+console.table([acountID,acountEmail,acountPassword,acountCity, acountState])
+// acountState - undefined
+
+
+// prefer not to use var because of issue in block scope and functional scope
