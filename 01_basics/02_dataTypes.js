fbde2169c4871002012e062d94a9cb7df516f3ac
Shiwam Yadav <developershiwam1569@gmail.com>
2025-07-12 05:21:07 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(30+40) // we are using node.js not browser so it will so ReferenceError: alert is not defined
+
+let name ="Shiwam"
+let age =25
+let isLogin= false
+let state;
+
+//number=> 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof name);
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
