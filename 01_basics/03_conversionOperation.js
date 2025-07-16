eb19d0b10ba547de7fb2d41a85a10105c7752003
Shiwam Yadav <developershiwam1569@gmail.com>
2025-07-16 21:34:15 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+//let score =1569
+
+// let score ="1569"
+
+// let newscore = Number(score)
+// console.log(typeof newscore) // string
+
+let score ="123abc"
+
+let newscore = Number(score)
+console.log(typeof newscore) // number
+//but
+console.log(newscore) // Nan
+
+let a = null
+console.log(typeof a) // object
+console.log(a) //null
+
+let b=Number(a)
+console.log(b) // 0
+
+let c = undefined
+console.log(typeof c)
+console.log(c)
+
+let d=Number(c)
+console.log(d)
+
+let isLoggedin = 1
+
+let isbooleanloggein= Boolean(isLoggedin)
+console.log(isbooleanloggein) // true
+
+let isLogg = "dcbhjb"
+
+let isbooleanlogg= Boolean(isLogg)
+console.log(isbooleanlogg) // true
+
+// 1=> true 0=> false ""=>false "dcbjhsb"=>true
+
+let abc= 12
+let bcd=String(abc)
+console.log(bcd)
+
+console.log(typeof bcd) //string
+
